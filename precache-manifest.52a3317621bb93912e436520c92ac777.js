7bd3dc4b40077420c3c5b9328477e507aaa50279
Devon Lewis <theekidfresco@gmail.com>
2020-09-27 10:11:26 -0600

    Updates

---
+++ precache-manifest.52a3317621bb93912e436520c92ac777.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64726ddb85570a0254d67348d26b564b",
+    "url": "/gigstarter/index.html"
+  },
+  {
+    "revision": "0f0049a41339052dbc01",
+    "url": "/gigstarter/static/css/main.0875db77.chunk.css"
+  },
+  {
+    "revision": "d67ca56a03ecc6a44b7f",
+    "url": "/gigstarter/static/js/2.16e46f73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gigstarter/static/js/2.16e46f73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f0049a41339052dbc01",
+    "url": "/gigstarter/static/js/main.04d7540a.chunk.js"
+  },
+  {
+    "revision": "ff4c2c1598d4ec208f09",
+    "url": "/gigstarter/static/js/runtime-main.d50789dc.js"
+  },
+  {
+    "revision": "7c1f787948b37b35f10740ec7d874bd2",
+    "url": "/gigstarter/static/media/back2.7c1f7879.jpg"
+  },
+  {
+    "revision": "12bce3f0a25c50db9228a7bdb6025b70",
+    "url": "/gigstarter/static/media/businessman.12bce3f0.svg"
+  },
+  {
+    "revision": "f783850ed5c43d019a14cd157de7769d",
+    "url": "/gigstarter/static/media/employee.f783850e.svg"
+  },
+  {
+    "revision": "8796c6c1ae29b2fa88d7a7df37124a70",
+    "url": "/gigstarter/static/media/envelope.8796c6c1.svg"
+  },
+  {
+    "revision": "c0cd138271276bb33a87c2d174015455",
+    "url": "/gigstarter/static/media/facebook.c0cd1382.svg"
+  },
+  {
+    "revision": "3797155aa59a293bf7ead44c95e14fd0",
+    "url": "/gigstarter/static/media/greenind.3797155a.svg"
+  },
+  {
+    "revision": "8082cb02040074a64a96c04ba22d1873",
+    "url": "/gigstarter/static/media/growth.8082cb02.svg"
+  },
+  {
+    "revision": "b1c58ee19d1e2c38b29ae86f3174f2e4",
+    "url": "/gigstarter/static/media/instagram.b1c58ee1.svg"
+  },
+  {
+    "revision": "4b475fb3f54b3e35daa04295ec48df4c",
+    "url": "/gigstarter/static/media/linkedin.4b475fb3.svg"
+  },
+  {
+    "revision": "b86262bb1045a2b16a4d9fcf64afc1b1",
+    "url": "/gigstarter/static/media/money-bag.b86262bb.svg"
+  },
+  {
+    "revision": "3cd45c1771449b0e1a9ea1303f7944f4",
+    "url": "/gigstarter/static/media/orangeind.3cd45c17.svg"
+  },
+  {
+    "revision": "6e7e061fe41a357be7086cd2e78bf67f",
+    "url": "/gigstarter/static/media/presentation.6e7e061f.svg"
+  },
+  {
+    "revision": "70d7efe5080a8950bc1028e7f52b2ecb",
+    "url": "/gigstarter/static/media/search.70d7efe5.svg"
+  },
+  {
+    "revision": "8709a2d928b6cada17181eddf41e172e",
+    "url": "/gigstarter/static/media/shop.8709a2d9.svg"
+  },
+  {
+    "revision": "182d5ef4d173a3cd93897242a6fd56a5",
+    "url": "/gigstarter/static/media/twitter.182d5ef4.svg"
+  }
+]);
